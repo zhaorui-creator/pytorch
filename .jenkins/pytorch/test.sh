e0caa181bd4aed9c01cd509b0e3c0fbf6f160dc3
Edward Z. Yang <ezyang@fb.com>
2019-05-22 11:45:56 -0400

    Pin torchvision version.
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>
    
    gh-metadata: pytorch pytorch 20811 gh/ezyang/138/head

--- .jenkins/pytorch/test.sh
+++ .jenkins/pytorch/test.sh
@@ -146,12 +146,13 @@ test_torchvision() {
   # PyTorch CI
   git clone https://github.com/pytorch/vision --quiet
   pushd vision
+  git checkout 76b2667958f994b7d942645285f777046d7d2d16
   # python setup.py install with a tqdm dependency is broken in the
   # Travis Python nightly (but not in latest Python nightlies, so
   # this should be a transient requirement...)
   # See https://github.com/pytorch/pytorch/issues/7525
   #time python setup.py install
-  pip install -q --user .
+  pip install --user .
   popd
   rm -rf vision
 }
